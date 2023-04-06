c18b5e3f471266b2ba518ee791d297dd36498ee8
chickenchickenlove <ojt90902@naver.com>
2023-04-06 23:35:11 +0900

    implementation getter function

--- getter.py
+++ getter.py
@@ -4,67 +4,60 @@ import aiohttp
 import dateparser
 
 from collections import defaultdict
-
 from secret import ACCESS_TOKEN, BLOG_NAME, OUTPUT
 
+BASE_URL = f'https://www.tistory.com/apis/post/list?access_token={ACCESS_TOKEN}&blogName={BLOG_NAME}&output={OUTPUT}&count=30'
+URL_WITH_PAGE = BASE_URL + '&page={}'
 
 
-base_url = f'https://www.tistory.com/apis/post/list?access_token={ACCESS_TOKEN}&blogName={BLOG_NAME}&output={OUTPUT}&count=30'
-url_with_page = base_url + '&page={}'
+async def parse_data(history_dict: defaultdict,
+                     res: aiohttp.ClientResponse) -> bool:
+    if res.status != 200:
+        return False
 
-async def get_post_data(post_history, start_index):
+    text = await res.text()
+    contents = json.loads(text)
+
+    # in case of having not 'posts'
+    items = contents['tistory']['item']
+    if 'posts' not in items:
+        return False
+
+    for post in items['posts']:
+        parsed_date = dateparser.parse(post['date'])
+        isoweekday = parsed_date.isoweekday()
+
+        history_dict[parsed_date.date()] += 1
+    return True
+
+
+async def get_post_data(session: aiohttp.ClientSession,
+                        history_dict: defaultdict,
+                        start_page: int) -> None:
+    do_continue = True
+
+    futures = [session.request(
+        method='get',
+        url=URL_WITH_PAGE.format(page_number)) for page_number in range(start_page, start_page + 5)]
+    responses = [await f for f in futures]
+
+    for res in responses:
+        do_continue = await parse_data(history_dict, res)
+
+    if do_continue:
+        await get_post_data(session, history_dict, start_page + 5)
+
+
+async def do_request() -> defaultdict:
+    history_dict = defaultdict(int)
 
     async with aiohttp.ClientSession() as session:
-        futures = [session.request(
-                        method='get',
-                        url=url_with_page.format(index)) for index in range(start_index, start_index + 5)]
+        await get_post_data(session, history_dict, 1)
 
-        responses = [await f for f in futures]
-
-        flag = True
-
-        for res in responses:
-
-            if res.status != 200:
-                flag = False
-                continue
-
-            text = await res.text()
-            contents = json.loads(text)
-
-            #  post가 없을 수도 있음.
-            items = contents['tistory']['item']
-            if 'posts' not in items:
-                flag = False
-                continue
-
-            posts = contents['tistory']['item']['posts']
-
-
-            for post in posts:
-                d = post['date']
-                parsed_date = dateparser.parse(d)
-                isoweekday = parsed_date.isoweekday()
-
-                post_history[parsed_date.date()] += 1
-
-        print(post_history)
-        if flag :
-            await get_post_data(post_history, start_index + 5)
+    return history_dict
 
 
 
 
+# post_history = asyncio.run(do_request())
 
-
-async def do_request():
-    post_history = defaultdict(int)
-    await get_post_data(post_history, 1)
-    print(post_history)
-    print("ASH1")
-
-asyncio.run(do_request())
-
-
-
-print("ASH2")
